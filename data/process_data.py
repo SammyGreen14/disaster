0b805a99cf201d074542aeeed58576685569f17a
SammyGreen14 <105079126+SammyGreen14@users.noreply.github.com>
2022-07-12 11:41:47 -0400

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -51,6 +51,8 @@ def clean_data(df):
     df.drop(['categories'],axis =1, inplace = True)
     #Attach the cleaned category columns
     df = pd.concat([df, categories], axis = 1)
+    #Drop rows with related value of 2
+    df = df[df['related'] >= 2]
     #Remove any duplicate rows
     df = df.drop_duplicates()
     
